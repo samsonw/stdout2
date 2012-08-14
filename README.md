3ba267b3478c2b75b7acb6e86febb8864a2a68fb
Samson Wu <samson959@gmail.com>
2012-08-14 17:12:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Intro
+This is the app code behind [http://stdout.samsonw.info/](http://stdout.samsonw.info/).
+
+## Config
+
+## Deployment
+
+## Customization
+
+Have fun!
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stdout2::Application.config.secret_token = '0e892e18458b3a420ece8522d9b9a899a874a519bfaef839b19f291b5cf91cf902fcaa3656f055ed5777306c983f21b3436b8c9b0ada581589fa674ca2801d58'
